2ffb69a9d6ceba794f80becac3dfe3121b8fd803
avman <avoyan.mane@mail.ru>
2024-03-02 19:13:52 +0400

    Some changes in matrix_operations.py

--- matrix_operations.py
+++ matrix_operations.py
@@ -1,22 +1,21 @@
 import random
 
-
 def generate_random_matrix(rows, cols):
     matrix = []
     for row in range(rows):
-        matrix.append([random.randint(1, 100) for i in range(cols)])
+        matrix.append([random.randint(1, 100) for _ in range(cols)])
     return matrix
 
 
 def get_column_sum(mat, col_ind):
-    return sum(list(mat[i][col_ind] for i in range(len(mat))))
+    return sum(mat[i][col_ind] for i in range(len(mat)))
 
 
 def get_row_average(mat, row_index):
-    return sum(list(mat[row_index][i] for i in range(len(mat))))/len(mat[0])
+    return sum(mat[row_index][i] for i in range(len(mat[0]))) / len(mat[0])
 
 
-mat = generate_random_matrix(3, 4)
+mat = generate_random_matrix(4, 3)
 print(mat)
-print(get_column_sum(mat, 3))
+print(get_column_sum(mat, 0))
 print(get_row_average(mat, 0))
